042d178ffcfb4cc2a68526451256e967f8b30268
Uha <shravya.g0311@gmail.com>
2025-07-17 16:08:02 +0530

    Added Addition program

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition{
+	public static int add(int a,int b){
+		return a+b;
+	}
+	public static void main(String[] args){
+		int sum=add(2,3);
+		System.out.println("Sum is"+sum);
+	}
+}
